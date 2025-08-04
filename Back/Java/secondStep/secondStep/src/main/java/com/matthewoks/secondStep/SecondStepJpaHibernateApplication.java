555df7361ff3b02bc3e911ff00a0308615c92699
Matteo <101025360+Matthewoks@users.noreply.github.com>
2025-08-04 02:12:16 +0200

    nuovo progetto back spring livello superiore

---
+++ Back/Java/secondStep/secondStep/src/main/java/com/matthewoks/secondStep/SecondStepJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.matthewoks.secondStep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondStepJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondStepJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ Back/Java/secondStep/secondStep/src/test/java/com/matthewoks/secondStep/SecondStepJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthewoks.secondStep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondStepJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
