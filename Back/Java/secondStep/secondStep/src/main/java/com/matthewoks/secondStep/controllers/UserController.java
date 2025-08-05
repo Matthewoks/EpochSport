a5085ef4d247bd13c24ed517ab8f6618aa151d41
Matteo <101025360+Matthewoks@users.noreply.github.com>
2025-08-06 01:29:59 +0200

    Repo JPA with @Query

--- Back/Java/secondStep/secondStep/src/main/java/com/matthewoks/secondStep/controllers/UserController.java
+++ Back/Java/secondStep/secondStep/src/main/java/com/matthewoks/secondStep/controllers/UserController.java
@@ -3,11 +3,10 @@ package com.matthewoks.secondStep.controllers;
 import com.matthewoks.secondStep.models.User;
 import com.matthewoks.secondStep.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/users")
@@ -18,5 +17,37 @@ public class UserController {
     @GetMapping
     public List<User> getAll(){
         return repo.findAll(); //già disponibile senza fare nulla di particolare nella repo
+    }
+
+    @GetMapping("/username/{username}")
+    public User getByUserName(@PathVariable String username){
+        return repo.findByUsername(username); //già disponibile senza fare nulla di particolare nella repo
+        //username è univoco solo un oggetto
+    }
+
+    @GetMapping("/language_preference/{lang}")
+    public List<User> getByLanguagePreference(@PathVariable String lang){
+        return repo.findByLanguagePreference(lang); //già disponibile senza fare nulla di particolare nella repo
+    }
+
+    @GetMapping("/fragmentusername/{username}")
+    public List<User> getByFragmentUsername(@PathVariable String username){
+        return repo.findByUsernameFragment(username); //già disponibile senza fare nulla di particolare nella repo
+    }
+
+    @GetMapping("/combined/{username}/{lang}")
+    public User getByUsernameAndLanguagePreference(@PathVariable String username, @PathVariable String lang){
+        return repo.findByUsernameAndLanguagePreference(username,lang); //già disponibile senza fare nulla di particolare nella repo
+    }//ricerca combinata
+
+    @GetMapping("/{id}")
+    public Optional<User> getById(@PathVariable int id){
+        return repo.findById((long)id); //già disponibile senza fare nulla di particolare nella repo
+        //se non utilizzo Optional return repo.findById((long)id).orElse(null);
+    }
+
+    @PostMapping
+    public User save(@RequestBody User us){
+        return repo.save(us); //salva e rende l'oggetto creato
     }
 }
--- Back/Java/secondStep/secondStep/src/main/java/com/matthewoks/secondStep/repositories/UserRepository.java
+++ Back/Java/secondStep/secondStep/src/main/java/com/matthewoks/secondStep/repositories/UserRepository.java
@@ -2,8 +2,21 @@ package com.matthewoks.secondStep.repositories;
 
 import com.matthewoks.secondStep.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository  extends JpaRepository<User,Long> {
+
+    User findByUsername(String username);
+    User findByUsernameAndLanguagePreference(String username, String lang);
+
+    @Query("SELECT s FROM User s WHERE username LIKE %:usrn%") //jpa non guarda i nomii delle colonne ma la classe
+    List<User> findByUsernameFragment(@Param("usrn") String username);
+    List<User> findByLanguagePreference(String lang);
+
+
 }
